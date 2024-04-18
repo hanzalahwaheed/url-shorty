c6b7226ce2f9d31d344992e9d9e6b04c3f43b016
hanzalahwaheed <hanzalah.w@gmail.com>
2024-04-18 15:21:07 +0400

    added default get request API

--- server/routes/url.js
+++ server/routes/url.js
@@ -2,6 +2,7 @@ import express from "express";
 import { generateShortUrl, getShortUrl } from "../controllers/url.js";
 const router = express.Router();
 
+router.get("/", (req, res) => res.json("default get request"));
 router.post("/", generateShortUrl);
 router.get("/:shortId", getShortUrl);
 
